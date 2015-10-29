2776e194988583b30396e4764814237abbb5f101
Reinaert Van de Cruys <reinaert.vandecruys@gmail.com>
2015-10-29 18:02:19 +0100

    Make basic abstract Weapon implementation.

--- server/src/Ship.java
+++ server/src/Ship.java
@@ -21,7 +21,9 @@ public abstract class Ship {
     }
 
     public void hit() {
-        mHitPoints--;
+        if (mHitPoints > 0) {
+            mHitPoints--;
+        }
     }
 
     public boolean isSunk() {
--- server/src/Weapon.java
+++ server/src/Weapon.java
@@ -1,2 +1,46 @@
-public class Weapon {
+import java.util.Observable;
+
+public abstract class Weapon extends Observable {
+    protected Board mBoard;
+    private int mCooldown;
+    private int mCurrentCooldown;
+
+    public Weapon(Board board, int cooldown, int currentCooldown) {
+        mBoard = board;
+        mCooldown = cooldown;
+        mCurrentCooldown = currentCooldown;
+    }
+
+    public void coolDown() {
+        if (mCurrentCooldown > 0) {
+            mCurrentCooldown--;
+        }
+    }
+
+    public void deploy(Field field, Coordinate coordinate) throws NotReadyException {
+        if (!isReady()) {
+            throw new NotReadyException();
+        } else {
+            _deploy(field, coordinate);
+            mCurrentCooldown = mCooldown;
+        }
+    }
+
+    protected abstract void _deploy(Field field, Coordinate coordinate);
+
+    public int getCooldown() {
+        return mCooldown;
+    }
+
+    public int getCurrentCooldown() {
+        return mCurrentCooldown;
+    }
+
+    public boolean isReady() {
+        return mCooldown <= 0;
+    }
+
+    public class NotReadyException extends Exception {
+        NotReadyException() {}
+    }
 }
