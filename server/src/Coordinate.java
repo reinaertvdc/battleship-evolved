b8e1319508b0778372ddebb3713b971facaee301
Reinaert Van de Cruys <reinaert.vandecruys@gmail.com>
2015-10-29 17:32:27 +0100

    Make basic Coordinate and abstract Ship implementation.

--- server/src/Coordinate.java
+++ server/src/Coordinate.java
@@ -1,2 +1,17 @@
 public class Coordinate {
+    private int mRow;
+    private int mColumn;
+
+    public Coordinate(int row, int column) {
+        mRow = row;
+        mColumn = column;
+    }
+
+    public int getRow() {
+        return mRow;
+    }
+
+    public int getColumn() {
+        return mColumn;
+    }
 }
--- server/src/Ship.java
+++ server/src/Ship.java
@@ -1,2 +1,30 @@
-public class Ship {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Ship {
+    private Coordinate mSize;
+    private int mHitPoints;
+    protected Weapon[] mWeapons;
+
+    protected Ship(Coordinate size) {
+        mSize = size;
+        mHitPoints = size.getRow() * size.getColumn();
+        mWeapons = new Weapon[0];
+    }
+
+    public Coordinate getSize() {
+        return mSize;
+    }
+
+    public Weapon[] getWeapons() {
+        return mWeapons;
+    }
+
+    public void hit() {
+        mHitPoints--;
+    }
+
+    public boolean isSunk() {
+        return mHitPoints > 0;
+    }
 }
