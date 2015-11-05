e9bfbb2071ee2ca076e08f5f68f05580499b3254
Jelco Adamczyk <jelcobel@hotmail.com>
2015-11-05 19:54:31 +0100

    Started implementation for Game and Playerclass

--- shared/src/main/java/be/uhasselt/ttui/battleshipevolved/Game.java
+++ shared/src/main/java/be/uhasselt/ttui/battleshipevolved/Game.java
@@ -1,4 +1,36 @@
 package be.uhasselt.ttui.battleshipevolved;
 
+import java.util.ArrayList;
+
+/**
+ *  The Game class, contains the main game loop
+ *  @author Jelco Adamczyk
+ */
 public class Game {
+    private final static int maxPlayers = 4;
+    private Board mBoard;
+    private ArrayList<Player> mPlayers;
+    private int mTurn;
+
+    public Game() {
+
+        //create 4 players and init
+        mPlayers = new ArrayList<>();
+        for (int i = 0; i < maxPlayers; i++) {
+            Player player = new Player();
+            mPlayers.add(player);
+        }
+        //set the turn to player 1
+        mTurn = 0;
+        //init the board
+        mBoard = new Board((Player[]) mPlayers.toArray());
+    }
+
+    public void placementTurn() {
+        Player player = mPlayers.get(mTurn);
+        Coordinate co = new Coordinate(0,0);
+        player.placeBattleShip(mBoard, co, true);
+    }
+
+
 }
--- shared/src/main/java/be/uhasselt/ttui/battleshipevolved/Player.java
+++ shared/src/main/java/be/uhasselt/ttui/battleshipevolved/Player.java
@@ -1,4 +1,23 @@
 package be.uhasselt.ttui.battleshipevolved;
 
+import java.util.ArrayList;
+
 public class Player {
+    private Field mField;
+    private ArrayList<Ship> mShips;
+
+    public Player() {
+        mField = new Field();
+        mShips = new ArrayList<>();
+    }
+
+    public void placeBattleShip(Board board, Coordinate coordinate, boolean liesHorizontal) {
+        ShipBattleship ship = new ShipBattleship(board);
+        mField.deployShip(ship, coordinate, liesHorizontal);
+    }
+
+
+    public Field getField() {
+        return mField;
+    }
 }
