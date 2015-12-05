527e975d7d00657aa19dbaef772ae44f66093bdb
Tremnor <owner_ke@hotmail.com>
2015-12-05 22:15:10 +0100

    Implemented the start of the serversocket
    Next step is to edit the client side and preform a simple handshake.

--- server/src/main/java/be/uhasselt/ttui/battleshipevolved/server/Main.java
+++ server/src/main/java/be/uhasselt/ttui/battleshipevolved/server/Main.java
@@ -1,5 +1,9 @@
 package be.uhasselt.ttui.battleshipevolved.server;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 
 import be.uhasselt.ttui.battleshipevolved.Game;
@@ -8,9 +12,43 @@ import be.uhasselt.ttui.battleshipevolved.Game;
  * Main is the entry point for the Battleship Evolved server.
  */
 public class Main {
-    private static ArrayList<String> mStringlijst;
+    public final static int PLAYERAMOUNT = 4;
+    private static ArrayList<Socket> mPlayers;
+
     public static void main(String[] args) {
-        Game game = new Game();
-        System.out.println("The server works!");
+        ServerSocket serverSocket = null;
+        mPlayers = new ArrayList<>();
+        int count = 0;
+
+        try {
+            serverSocket = new ServerSocket(4004);
+            System.out.println("The server started and listens to port 4004.");
+            while(count < PLAYERAMOUNT) {
+                Socket playerSocket = serverSocket.accept();
+                System.out.println("Client " + count + " connected.");
+                PlayerServer playerServer = new PlayerServer(playerSocket, count);
+                playerServer.start();
+                mPlayers.add(playerSocket);
+                count++;
+                System.out.println("Client " + count + " saved.");
+                updateAmount();
+                System.out.println("Clients notified.");
+            }
+            Game game = new Game();
+            serverSocket.close();
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 4004");
+        }
+    }
+
+    private static void updateAmount() {
+        try {
+            for (int i = 0; i < mPlayers.size(); i++) {
+                PrintWriter outp = new PrintWriter(mPlayers.get(i).getOutputStream(), true);
+                outp.println("Players " + mPlayers.size() + " " + PLAYERAMOUNT);
+            }
+        } catch (IOException e) {
+            System.err.println("Could not send to client.");
+        }
     }
 }
---
+++ server/src/main/java/be/uhasselt/ttui/battleshipevolved/server/PlayerServer.java
@@ -0,0 +1,21 @@
+package be.uhasselt.ttui.battleshipevolved.server;
+
+import java.net.Socket;
+
+/**
+ * A thread that handles everything from a single player
+ * @author Arno Stienaers
+ */
+public class PlayerServer extends Thread {
+    private Socket mSocket = null;
+    private int mID;
+
+    public PlayerServer(Socket socket, int id) {
+        mSocket = socket;
+        mID = id;
+    }
+
+    public void run() {
+
+    }
+}
