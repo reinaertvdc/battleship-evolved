d6c06f16adaf434db443e45c2becb1ebf3ba40da
Tremnor <owner_ke@hotmail.com>
2015-12-17 03:22:27 +0100

    Tried to add audio, failed in finding path?
    Feel free to help

---
+++ shared/src/main/java/be/uhasselt/ttui/battleshipevolved/Audio.java
@@ -0,0 +1,56 @@
+package be.uhasselt.ttui.battleshipevolved;
+
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.sound.sampled.Clip;
+
+/**
+ * Created by Arno on 17/12/2015.
+ */
+public class Audio implements Observer {
+    private ArrayList<String> mAudioHitPaths;
+    private ArrayList<String> mAudioWaterPaths;
+    private ArrayList<String> mAudioDiscoveredPaths;
+
+    public Audio() {
+        mAudioHitPaths = new ArrayList<String>();
+        mAudioHitPaths.add("audio/Missile1");
+        mAudioWaterPaths = new ArrayList<String>();
+        mAudioWaterPaths.add("audio/Water1");
+        mAudioDiscoveredPaths = new ArrayList<String>();
+        mAudioDiscoveredPaths.add("audio/Hint");
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if (arg instanceof CoordinateStatus) {
+            CoordinateStatus cStatus = (CoordinateStatus) arg;
+            playSound(cStatus.getStatus());
+        } else if (arg instanceof ArrayList<?>) {
+            for (Object obj : (ArrayList<?>) arg)
+                if (obj instanceof CoordinateStatus) {
+                    CoordinateStatus cStatus = (CoordinateStatus) obj;
+                    playSound(cStatus.getStatus());
+                }
+        }
+    }
+
+    private void playSound(CoordinateStatus.Status type) {
+        AudioPlayer player = new AudioPlayer();
+        switch (type) {
+            case FOG:
+                break;
+            case HIT:
+                player.play(mAudioHitPaths.get(0));
+                break;
+            case MISSED:
+                player.play(mAudioWaterPaths.get(0));
+                break;
+            case BOAT:
+                player.play(mAudioDiscoveredPaths.get(0));
+                break;
+        }
+    }
+}
